ed9ebccf87ea6e1c969d1116b9697a67112c1cd2
tkellogg <timothy.kellogg@gmail.com>
2012-08-03 20:01:23 -0600

    Added functionality for Record
    Building out the Database

---
+++ Jump.Location.Specs/RecordSpec.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Should;
+using Xunit;
+using Xunit.Extensions;
+
+namespace Jump.Location.Specs
+{
+    public class RecordSpec
+    {
+        public class DescribeFullName
+        {
+            [Fact]
+            public void It_splits_the_supplied_path_on_the_cons()
+            {
+                var record = new Record("provider::path", 0);
+                record.Provider.ShouldEqual("provider");
+                record.Path.ShouldEqual("path");
+            }
+
+            [Theory]
+            [InlineData("C:\\debug\\")]
+            [InlineData("")]
+            [InlineData("::")]
+            [InlineData("::C:\\debug")]
+            [InlineData("C:\\debug::")]
+            public void It_throws_ArgumentException_when_path_is_invalid(string path)
+            {
+                Assert.Throws<ArgumentException>(() => new Record(path, 0));
+            }
+        }
+    }
+}
