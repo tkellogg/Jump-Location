a4b131f4175cc9dca9740f21566f97b940ce43e5
tkellogg <timothy.kellogg@gmail.com>
2012-08-18 08:54:31 -0600

    Prevented double-loading
    Test to see if Jump-Location is already loaded before haphazardly
    cramming it in

--- Jump.Location/Load.ps1
+++ Jump.Location/Load.ps1
@@ -9,20 +9,25 @@
 $fullpath = Split-Path -Parent $MyInvocation.MyCommand.Path
 $dllpath = $fullpath + "\Jump.Location.dll"
 
-Import-Module $dllpath -Global -DisableNameChecking
-New-Alias -Name j -Value Jump-Location -Scope Global
+if (-Not (Get-Command Jump-Location -ErrorAction SilentlyContinue)) {
 
-function global:Jumpstat() {
-	Jump-Location -Status
+	Import-Module $dllpath -Global -DisableNameChecking
+	New-Alias -Name j -Value Jump-Location -Scope Global
+
+	function global:Jumpstat() {
+		Jump-Location -Status
+	}
+
+	function global:PushJ {
+		Param (
+			[Parameter(ValueFromRemainingArguments=$true)] $args
+		)
+		Jump-Location @args -Push
+	}
+
+	Jump-Location -Initialize
+
+	& $($fullpath + "\TabExpansion.ps1")
+} else {
+	Write-Warning "Jump-Location already loaded"
 }
-
-function global:PushJ {
-	Param (
-		[Parameter(ValueFromRemainingArguments=$true)] $args
-	)
-	Jump-Location @args -Push
-}
-
-Jump-Location -Initialize
-
-& $($fullpath + "\TabExpansion.ps1")
