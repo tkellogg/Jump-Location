5dfeacd2e5f57f2d824bc870b683746b3a6314d1
tkellogg <timothy.kellogg@gmail.com>
2012-10-10 21:50:00 -0600

    Set the version
    And some other assembly info

--- Jump.Location/Properties/AssemblyInfo.cs
+++ Jump.Location/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Jump.Location")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Powershell Cmdlet to jump around")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Hewlett-Packard")]
+[assembly: AssemblyCompany("Tim Kellogg")]
 [assembly: AssemblyProduct("Jump.Location")]
-[assembly: AssemblyCopyright("Copyright © Hewlett-Packard 2012")]
+[assembly: AssemblyCopyright("Copyright © Tim Kellogg 2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,6 +32,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.4.1.0")]
+[assembly: AssemblyFileVersion("0.4.1.0")]
 [assembly: InternalsVisibleTo("Jump.Location.Specs")]
