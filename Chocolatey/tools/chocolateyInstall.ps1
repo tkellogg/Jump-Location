8f7a1e1a3df5a1244de92313d38487006e556566
LCHarold <lcharold@hotmail.com>
2014-09-21 09:35:59 -0500

    add support for Chocolatey package creation

---
+++ Chocolatey/tools/chocolateyInstall.ps1
@@ -0,0 +1,8 @@
+﻿$name   = "Jump-Location"
+$url    = "http://sourceforge.net/projects/jumplocation/files/Jump-Location-0.5.1.zip/download"
+$unzipLocation = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+Install-ChocolateyZipPackage $name $url $unzipLocation
+
+$installer = Join-Path $unziplocation 'Install.ps1'
+& $installer
